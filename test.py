df1cd4ca4f5a97d705484a6fec539591e8ccc43c
IceSword8492 <cleared.for.approach.runway23@gmail.com>
2019-12-17 15:58:41 +0900

    updated

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 import numpy as np
-
-print(np.array([0, 1]))
+import pandas as pd
+np.random.seed(0)
+columns = ["apple", "orange", "banana", "strawberry", "kiwifruit"]
+df = pd.DataFrame()
+for column in columns:
+    df[column] = np.random.choice(range(1, 11), 10)
+df.index = range(1, 11)
+print(df.loc[(df["apple"] >= 5) & (df["kiwifruit"] >= 5)])
